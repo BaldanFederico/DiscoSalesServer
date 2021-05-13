ceb1742e88fb1eb610c60efd382e949fcfb1afc2
manue <manue@DESKTOP-D4E17EE.homenet.telecomitalia.it>
2021-05-13 18:24:12 +0200

    Fixed code lines

--- src/server/Utente.java
+++ src/server/Utente.java
@@ -5,10 +5,69 @@
  */
 package server;
 
+import java.util.Scanner;
+
 /**
  *
- * @author manue
+ * @author agostinelli.luca
+ * @author parrarodriguez.manue
  */
 public class Utente {
+
+    private String nome;
+    private String password;
+    private String Email;
+    private String codice;
+    private boolean stato;
+
+  
+
+    public Utente(String nome, String password, String Email, String codice, boolean stato) {
+        this.nome = nome;
+        this.password = password;
+        this.Email = Email;
+        this.codice = codice;
+        this.stato = stato;
+    }
+
+    public String getCodice() {
+        return codice;
+    }
+
+    public boolean getStato() {
+        return stato;
+    }
+
+    public void setStato(boolean stato) {
+        this.stato = stato;
+    }
+
     
+
+    public String getNome() {
+
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPassword() {
+
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
 }
