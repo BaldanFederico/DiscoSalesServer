5c0e11578c41560b19b7ee64badb4203886db8b4
parrarodriguez.manue <parrarodriguez.manue@TAVE-PC14.OSDBSESTO.local>
2021-05-13 13:51:09 +0200

    Create DiscoSales client application without GUI

--- src/socketbase/gestioneChatRoom.java
+++ src/socketbase/gestioneChatRoom.java
@@ -5,17 +5,9 @@
  */
 package socketbase;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.util.Vector;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 /**
  *
