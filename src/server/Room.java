f281b24f5d433b6646f74f7fe4d3b0779206928d
manue <manue@DESKTOP-D4E17EE.homenet.telecomitalia.it>
2021-05-13 18:17:45 +0200

    Fixed code lines

---
+++ src/server/Room.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.net.Socket;
+import java.util.*;
+
+/**
+ *
+ * @author dell
+ */
+public class Room {
+
+    private Socket clientSocket;
+    private String nomeRoom;
+    private String owner;//un id univoco per ogni utente
+    private String RoomID; //un id univoco per la room
+    private String partecipante;
+
+    public Room(Socket clientSocket, String nomeRoom, String owner, String RoomID, String partecipante) {
+        this.clientSocket = clientSocket;
+        this.nomeRoom = nomeRoom;
+        this.owner = owner;
+        this.RoomID = RoomID;
+        this.partecipante = partecipante;
+    }
+  
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getNomeRoom() {
+        return nomeRoom;
+    }
+
+    public String getRoomID() {
+        return RoomID;
+    }
+
+    public String getPartecipante() {
+        return partecipante;
+    }
+
+}
